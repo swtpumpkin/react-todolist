d60eeb75b2383ead6327bf81a92f1da10bfb7506
swtpumpkin <swtpumpkin90@gmail.com>
2018-01-17 14:36:56 +0900

    주석 추가

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
+/*
+  todos: todo 객체들이 들어있는 배열
+  onToggle: 체크박스를 키고 끄는 함수
+  onRemove: 아이템을 삭제시키는 함수
+*/
+
 class TodoItemList extends Component {
   
   shouldComponentUpdate(nextProps, nextState) {

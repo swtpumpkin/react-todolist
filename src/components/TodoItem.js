246811a123f75cb63080dcda1dd384d9f8d28230
swtpumpkin <swtpumpkin90@gmail.com>
2018-01-17 14:33:29 +0900

    todoitem 컴포넌트 최적화

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -10,6 +10,11 @@ import './TodoItem.css';
 */
 
 class TodoItem extends Component {
+  
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.checked !== nextProps.checked;
+  }
+
   render() {
     const { text, checked, id, onToggle, onRemove } = this.props;
 

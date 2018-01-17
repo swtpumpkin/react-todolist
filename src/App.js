3ac43ed8bf96f94775361858c274f5a912c52b99
swtpumpkin <swtpumpkin90@gmail.com>
2018-01-17 14:21:45 +0900

    제거 활성화

--- src/App.js
+++ src/App.js
@@ -57,13 +57,21 @@ class App extends Component {
     });
   }
 
+  handleRemove = id => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    });
+  }
+
   render() {
     const { input, todos } = this.state;
     const {
       handleCreate,
       handleChange,
       handleKeyPress,
-      handleToggle
+      handleToggle,
+      handleRemove
     } = this;
 
     return (
@@ -75,7 +83,7 @@ class App extends Component {
           onCreate={handleCreate}
         />
       )}>
-        <TodoItemList todos={todos} onToggle={handleToggle}/>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
       </TodoListTemplate>
     );
   }

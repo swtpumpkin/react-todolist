42de2e3dc6021b627dc738f6b9c7e459465db5c7
swtpumpkin <swtpumpkin90@gmail.com>
2018-01-17 12:44:15 +0900

    초기 셋팅 완료 및 템플릿 완성

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import TodoListTemplate from './components/TodoListTemplate';
+
+class App extends Component {
+  render() {
+    return (
+      <TodoListTemplate>
+        템플릿 완성
+      </TodoListTemplate>
+    );
+  }
+}
+
+export default App;
